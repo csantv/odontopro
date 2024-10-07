c14e83fcf4bc656a2fb91b6d550116f6ac473cb8
csantv <csantve@gmail.com>
2024-10-06 19:54:37 -0500

    fix eslint

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -1,28 +1,69 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// noinspection JSCheckFunctionSignatures
+
+import globals from "globals";
+import stylistic from "@stylistic/eslint-plugin";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import eslint from "@eslint/js";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    eslint.configs.recommended,
+    reactPlugin.configs.flat.recommended,
+    reactPlugin.configs.flat["jsx-runtime"],
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        ignores: ["**/dist/**"],
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    {
+        files: ["**/*.js"],
+        ...tseslint.configs.disableTypeChecked
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
-  },
-)
+    {
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: {
+                ...globals.browser,
+                ...globals.serviceworker,
+            },
+        },
+        plugins: {
+            "@stylistic": stylistic,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        settings: {
+            "react": {
+                "version": "detect"
+            }
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+            "@stylistic/quotes": ["error", "double"],
+            "@stylistic/semi": ["error", "always"],
+            "@stylistic/no-extra-semi": ["error"],
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/no-trailing-spaces": ["error"],
+            "@stylistic/eol-last": ["error", "always"],
+            "@stylistic/max-len": ["error", 120],
+            "eqeqeq": ["error", "smart"],
+            "@typescript-eslint/no-misused-promises": [
+                "error",
+                {
+                    checksVoidReturn: false,
+                },
+            ],
+        },
+    },
+);
