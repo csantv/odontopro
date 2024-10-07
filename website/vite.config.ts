c17e0658cde05a939c8380ba4c04f221da3ca0f3
Carlos Toro <csantve@gmail.com>
2024-10-07 08:45:22 -0500

    add license headers

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,3 +1,21 @@
+// OdontoPro - Dental Office System
+// Copyright (C) 2024-now  Carlos Toro
+//
+// This file is part of OdontoPro.
+//
+// OdontoPro is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OdontoPro is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OdontoPro.  If not, see <https://www.gnu.org/licenses/>.
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
