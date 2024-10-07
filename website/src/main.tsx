3c4f392448d9dcbf798ccd97f97848be5c6b7d63
csantv <csantve@gmail.com>
2024-10-06 22:35:56 -0500

    add some env variables

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -4,6 +4,8 @@ import App from "@/App.tsx";
 
 const root = document.getElementById("root");
 
+console.log(import.meta.env.VITE_API_BASE_URL);
+
 if (root != null) {
     createRoot(root).render(
         <StrictMode>
